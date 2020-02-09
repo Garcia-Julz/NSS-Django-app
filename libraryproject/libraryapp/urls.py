837347dd82a6234ba15031061109443cc4a41185
Julian Garcia <garcia.j.julian@gmail.com>
2020-02-09 16:42:51 -0600

    Setting up the Routes: completed

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from .views import *
 
 app_name = "libraryapp"
@@ -6,6 +6,7 @@ app_name = "libraryapp"
 urlpatterns = [
     path('', home, name='home'),
     path('books/', book_list, name='books'),
+    path('libraries/', libraries_list, name='libraries'),
     path('librarians/', librarians_list, name='librarians'),
-    path('libraries/', libraries_list, name='libraries')
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
