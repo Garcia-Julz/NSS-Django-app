fe996cd23a5981bf16c439167780ab4e1eed878d
Julian Garcia <garcia.j.julian@gmail.com>
2020-02-09 15:29:51 -0600

    added the library list

---
+++ libraryproject/libraryapp/views/libraries/list.py
@@ -0,0 +1,36 @@
+import sqlite3
+from django.shortcuts import render
+from libraryapp.models import Library
+from ..connection import Connection
+
+
+def libraries_list(request):
+    # if request.method == 'GET':
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            lib.title,
+            lib.address
+        from libraryapp_library lib;
+        """)
+
+        all_libraries = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            lib = Library()
+            lib.title = row["title"]
+            lib.address = row["address"]
+
+            all_libraries.append(lib)
+
+    template_name = 'libraries/list.html'
+
+    context = {
+        'all_libraries': all_libraries
+    }
+
+    return render(request, template_name, context)
